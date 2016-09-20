1998b438362655b6e3b1037e24ff94a5255e86b7
turtle <1639672490@qq.com>
2016-09-20 16:24:04 +0800

    update

--- run.sh
+++ run.sh
@@ -4,4 +4,3 @@ service php7.0-fpm start
 cd /var/www/laravel
 chmod -R 777 storage bootstrap/cache
 mysql -e "create database laravel default charset utf8"
-mysql laravel < /var/www/laravel/sqlfile.sql
